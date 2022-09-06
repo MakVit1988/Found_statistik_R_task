4aaee0104180c1cb7a066aa493feb8434acba711
MakVit1988 <elect.lucker@gmail.com>
2022-09-06 22:14:15 +0300

    Добавлено решение второй задачи через sapply

--- Found_statistik2_part_2.R
+++ Found_statistik2_part_2.R
@@ -8,3 +8,25 @@ get_coefficients <- function(dataset){
 }
 
 get_coefficients(test_data)
+
+
+# Задача 2
+test_data <- read.csv("https://stepic.org/media/attachments/course/524/cen_data.csv")
+
+var_names = c('X4', "X2", "X1")
+
+# Функция через for
+centered <- function(test_data, var_names){
+  for (i in 1:length(var_names)) {
+    test_data[,colnames(test_data)==var_names[i]] <- test_data[,colnames(test_data)==var_names[i]] -  mean(test_data[,colnames(test_data)==var_names[i]])
+  }
+  return(test_data)
+}
+
+# Решение через sapply
+centered <- function(test_data, var_names){
+  test_data[var_names] <- sapply(test_data[var_names], FUN = function(x) x-mean(x))
+  return(test_data)
+}
+
+centered(test_data, var_names)
