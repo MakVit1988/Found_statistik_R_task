7f145a56c4ca9c85b427c341961f4c7885e7874b
MakVit1988 <elect.lucker@gmail.com>
2022-09-11 19:47:13 +0300

    Добавлены альтернативные решения 7 задачи

--- Found_statistik2_part_2.R
+++ Found_statistik2_part_2.R
@@ -126,3 +126,36 @@ normality_by <- function(test){
 }
 
 normality_by(test_data)
+
+
+normality_by <- function(test){    
+  grouped_data <- aggregate(test[,1],by=list(test[,2], test[,3]),                                  
+                            FUN = function(x) {shapiro.test(x)$p.value})                                  
+  names(grouped_data) <- c(colnames(test)[2:3],'p_value')                                  
+  return(grouped_data)    
+}
+
+
+#Используя dplyr (при условии, что мы знаем имена переменных в данных):
+  
+  library(dplyr)    
+normality_by <- function(test_data){    
+  result <- test_data %>% group_by(y, z) %>%     
+    summarize(p_value = shapiro.test(x)$p.value)     
+  return(result)    
+}
+
+
+#Более общее решение с dplyr:
+  
+  library(dplyr)    
+get_p_value <- function(x){      
+  shapiro.test(x)$p.value    
+}    
+normality_by <- function(test){    
+  grouped <- test %>%    
+    group_by_(.dots = colnames(.)[2:3]) %>%         
+    summarise_each(funs(get_p_value))         
+  names(grouped)[3] <- 'p_value'         
+  return(grouped)         
+}
