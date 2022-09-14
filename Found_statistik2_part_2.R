32f3cdd163d3939aa647dcbbfb2efbd7c562328e
MakVit1988 <elect.lucker@gmail.com>
2022-09-14 20:18:57 +0300

    Добавлены примеры кластаризации

--- Found_statistik2_part_2.R
+++ Found_statistik2_part_2.R
@@ -191,3 +191,28 @@ ggplot() +
   geom_point(data = exemp, aes(X, Y))+
   geom_point(data = table_mean, aes(Xm ,Ym), col="red", size = 3)
 
+library(ggplot2) 
+library(ggrepel) # для симпатичной подписи точек на графике
+
+x <- rnorm(10)
+y <- rnorm(10)
+test_data <- data.frame(x, y)
+test_data$labels <- 1:10
+
+ggplot(test_data, aes(x, y, label = labels))+
+  geom_point()+
+  geom_text_repel()
+
+d = dist(test_data)
+fit <- hclust(d, method = "single")
+plot(fit, labels = test_data$labels)
+rect.hclust(fit, 5)
+
+
+library(ape)
+set.seed(222)
+tr <- rtree(20, tip.label = c("B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U")) 
+#левое дерево
+plot.phylo(tr) 
+#правое дерево 
+plot.phylo(tr, use.edge.length=FALSE)
